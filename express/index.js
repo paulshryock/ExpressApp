1b621e4f30e2f944bbd6f2983cd3ff6b4e422c7b
Paul Shryock <paul@pshry.com>
2019-02-10 19:56:30 -0500

    Bring back original express js file

--- express/index.js
+++ express/index.js
@@ -1,5 +1,28 @@
-const app = require('express')()
-app.get('*', (req, res) => {
-  res.send('Hello from Express.js!')
-})
-app.listen()
+'use strict';
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send( 'Hello world!' );
+});
+
+app.get('/api/courses', (req, res) => {
+
+	let data = [1, 2, 3];
+
+  res.send( data );
+});
+
+app.get('/api/courses/:id', (req, res) => {
+	// res.send(req.params);
+	// res.send(req.query);
+	res.send(req.params.id);
+});
+
+// To set a local bash environment variable:
+// `export PORT=5000`
+const port = process.env.PORT || 9000;
+app.listen(port, () => {
+	console.log( `Listening on port ${port}` );
+});

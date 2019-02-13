6f91d6a8fbb58360f5f808f68e57ff87026ee50d
Paul Shryock <paul@pshry.com>
2019-02-13 14:40:22 -0500

    Add /api/courses post method

--- express/app.js
+++ express/app.js
@@ -1,10 +1,19 @@
 'use strict';
 
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+const courses = [
+	{ id: 1, name: 'course1' },
+	{ id: 2, name: 'course2' },
+	{ id: 3, name: 'course3' },
+];
+
 app.get('/', (req, res) => {
-  res.send( 'Hello world!' );
+  res.send( `<p>Hello world, you\'re on port ${port}!</p>` );
 });
 
 app.get('/api/courses', (req, res) => {
@@ -14,10 +23,30 @@ app.get('/api/courses', (req, res) => {
   res.send( data );
 });
 
+app.post('/api/courses', (req, res) => {
+	const schema = {
+		name: Joi.string().min(3).required()
+	};
+
+	const result = Joi.validate(req.body, schema );
+
+	if (result.error) {
+		// 400 Bad Request
+		res.status(400).send(result.error.details[0].message);
+		return;
+	}
+	const course = {
+		id: courses.length + 1,
+		name: req.body.name
+	};
+	courses.push(course);
+	res.send(course);
+});
+
 app.get('/api/courses/:id', (req, res) => {
-	// res.send(req.params);
-	// res.send(req.query);
-	res.send(req.params.id);
+	const course = courses.find(c => c.id === parseInt(req.params.id));
+	if (!course) res.status(404).send('The course with the given ID was not found.');
+	res.send(course);
 });
 
 // To set a local bash environment variable:
